89b3b612b47b2388b967c1a1a610433cfdf2aba9
Liam <l.flynn2@live.co.uk>
2017-02-13 21:35:12 +0000

    Initial Commit

---
+++ PGNParser/PGNParser.h
@@ -0,0 +1,19 @@
+//
+//  PGNParser.h
+//  PGNParser
+//
+//  Created by Liam on 13/02/2017.
+//  Copyright © 2017 Tigerpixel Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PGNParser.
+FOUNDATION_EXPORT double PGNParserVersionNumber;
+
+//! Project version string for PGNParser.
+FOUNDATION_EXPORT const unsigned char PGNParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGNParser/PublicHeader.h>
+
+
